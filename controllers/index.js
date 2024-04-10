03142847fa88c72d8ce5d56c5e4379c2b4630da8
Mrbabet <108229726+Mrbabet@users.noreply.github.com>
2024-04-10 20:23:52 +0200

    Update controllers/index.js
    Co-authored-by: Kamil Mrowka <68347303+KamilMr@users.noreply.github.com>

--- controllers/index.js
+++ controllers/index.js
@@ -109,19 +109,18 @@ const remove = async (req, res, next) => {
   try {
     const result = await Contact.findOneAndDelete(contactId);
     if (result) {
-      res.json({
+      return res.json({
         status: "success",
         code: 200,
         data: { contact: result },
       });
-    } else {
+    }
       res.status(404).json({
         status: "error",
         code: 404,
         message: `Not found contact id: ${contactId}`,
         data: "Not Found",
       });
-    }
   } catch (e) {
     console.error(e);
     next(e);
