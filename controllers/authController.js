d4350b4034a3aeeae4dd198125aadd66ce9f0843
Mrbabet <wieliczkokamil@gmail.com>
2024-04-20 10:42:18 +0200

    processing avatar

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,9 +3,6 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { userSchema } = require("../validation/userSchemas");
 const gravatar = require("gravatar");
-const path = require("path");
-const fs = require("fs/promises");
-const jimp = require("jimp");
 
 require("dotenv").config();
 
@@ -117,35 +114,4 @@ const register = async (req, res) => {
   }
 };
 
-const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
-
-const updateAvatar = async (req, res) => {
-  const { path: tempUpload, originalname } = req.file;
-  const { _id: id } = req.user;
-  const imageName = `${id}_${originalname}`;
-
-  try {
-    const resultUpload = path.join(avatarsDir, imageName);
-    await fs.rename(tempUpload, resultUpload);
-    const avatarURL = path.join("public", "avatars", imageName);
-
-    jimp.read(avatarURL, (error, imageName) => {
-      if (error) throw error;
-      imageName.resize(250, 250).write(avatarURL);
-    });
-
-    await User.findByIdAndUpdate(id, { avatarURL });
-    res.json({
-      status: "success",
-      code: 200,
-      data: {
-        avatarURL,
-      },
-    });
-  } catch (error) {
-    await fs.unlink(tempUpload);
-    throw error;
-  }
-};
-
-module.exports = { getCurrent, login, register, logout, updateAvatar };
+module.exports = { getCurrent, login, register, logout };
